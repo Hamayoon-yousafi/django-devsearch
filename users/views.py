0df83070fc43c98dc2aef892a9f7ed83db01bf0c
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-03-12 16:44:03 +0430

    started video 18 user login, logout and flash messages

--- users/views.py
+++ users/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import Profile
+
+def login_page(request):
+    return render(request, 'users/login_register.html')
 
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    values = {
+        'profiles': Profile.objects.all()
+    }
+    return render(request, 'users/profiles.html', values)
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    values = {
+        'profile': profile,
+        'top_skills': profile.skill_set.exclude(description__exact=''),
+        'other_skills': profile.skill_set.filter(description='')
+    }
+    return render(request, 'users/user-profile.html', values)
