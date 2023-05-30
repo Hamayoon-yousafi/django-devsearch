3441533393fe50280687ce0e4f6f68aedd640cb4
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-05-30 11:54:57 +0430

    added jwt authentication. finsihed video 36 authentication with json web tokens

--- api/serializers.py
+++ api/serializers.py
@@ -24,9 +24,10 @@ class ProjectSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
     reviews = serializers.SerializerMethodField()
     # since we do not have reviews field in the Project model, we can define a new attribute in this serializer class and get all the related reviews for this project
+    
     class Meta:
         model = Project
-        fields = ['title', 'description', 'owner', 'tags', 'reviews']
+        fields = ['id', 'title', 'description', 'owner', 'tags', 'reviews']
 
     def get_reviews(self, obj): # obj referes to the particular project object
         reviews = obj.review_set.all()
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('', views.get_routes),
     path('projects/', views.get_projects),
     path('projects/<str:pk>', views.get_project),
+
+    # a token will be generated but it is refereshed every 5 mins or so
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
