a6c9f0b779cbc18437d0cdbd81fd743bd7244745
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-05-31 16:49:47 +0430

    finished video 37 sending post request

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('', views.get_routes),
     path('projects/', views.get_projects),
-    path('projects/<str:pk>', views.get_project),
+    path('projects/<str:pk>/', views.get_project),
+    path('projects/<str:pk>/vote/', views.project_vote),
 
     # a token will be generated but it is refereshed every 5 mins or so
     path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 @api_view(['GET'])
 def get_routes(request):
@@ -10,7 +10,7 @@ def get_routes(request):
         {'GET': '/api/projects'},
         {'GET': '/api/projects/id'},
         {'POST': '/api/projects/id/vote'},
-
+        # token routes. generate token and refresh token 
         {'POST': '/api/users/token'},
         {'POST': '/api/users/token/refresh'},
     ]
@@ -26,5 +26,26 @@ def get_projects(request):
 @api_view(['GET'])
 def get_project(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated]) # only login users can access this route
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    # get_or_create will check the database if such project exists, if not it creates it. The second variable (created) will be boolean and True if new instance is created.
+    review, created = Review.objects.create( 
+        owner=user,
+        project=project,
+    )
+
+    review.value = data.get('value')
+    review.body = data.get('body')
+    review.save()
+    project.get_vote_count
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
