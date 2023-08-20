173da1170dbeaeb3b391526550ebb4e1cd7720a2
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-08-20 11:44:49 +0430

    customized admin view

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_filter = ('owner', 'vote_total') # fields used for filtering
+    readonly_fields = ('vote_total',)
+    list_display = ('title', 'owner', 'vote_total') # fields which will be shown in the list view of Project
+
+
+admin.site.register(models.Project, ProjectAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Review)
